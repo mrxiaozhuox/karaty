36be674f9a5acb9704798da033770382b7e1ff68
YuKun Liu <mrxzx.info@gmail.com>
2022-09-19 10:43:41 -0700

    Merge remote-tracking branch 'refs/remotes/origin/main'
    Initial commit

---
+++ Karaty/KaratyApp.swift
@@ -0,0 +1,17 @@
+//
+//  KaratyApp.swift
+//  Karaty
+//
+//  Created by 刘禹琨 on 2022/9/18.
+//
+
+import SwiftUI
+
+@main
+struct KaratyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
