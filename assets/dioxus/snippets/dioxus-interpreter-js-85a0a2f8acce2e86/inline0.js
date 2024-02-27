8a69c1c472ba58420cdd6b08e9bfa4e1263f230e
mrxiaozhuox <mrxzx.info@gmail.com>
2024-02-27 09:52:16 +0000

    Deploying to gh-pages from @ mrxiaozhuox/karaty@a1e8dc236511253f2eaddcb0d83f7916678aeead 🚀

--- assets/dioxus/snippets/dioxus-interpreter-js-85a0a2f8acce2e86/inline0.js
+++ assets/dioxus/snippets/dioxus-interpreter-js-85a0a2f8acce2e86/inline0.js
@@ -181,18 +181,6 @@ let m,p,ls,d,t,op,i,e,z,metaflags;
                         else{
                             return attr[attr_tmp2&4294967167];
                         }
-                    }let u8buf,u8bufp;const evt = [];
-                    let evt_tmp1, evt_tmp2;
-                    function get_evt() {
-                        evt_tmp2 = u8buf[u8bufp++];
-                        if(evt_tmp2 & 128){
-                            evt_tmp1=s.substring(sp,sp+=u8buf[u8bufp++]);
-                            evt[evt_tmp2&4294967167]=evt_tmp1;
-                            return evt_tmp1;
-                        }
-                        else{
-                            return evt[evt_tmp2&4294967167];
-                        }
                     }const ns_cache = [];
                     let ns_cache_tmp1, ns_cache_tmp2;
                     function get_ns_cache() {
@@ -205,8 +193,20 @@ let m,p,ls,d,t,op,i,e,z,metaflags;
                         else{
                             return ns_cache[ns_cache_tmp2&4294967167];
                         }
-                    }let u32buf,u32bufp;let s = "";let lsp,sp,sl; let c = new TextDecoder();
-            let len,id,bubbles,ptr,ns,value,event_name,field;
+                    }let u32buf,u32bufp;let u8buf,u8bufp;const evt = [];
+                    let evt_tmp1, evt_tmp2;
+                    function get_evt() {
+                        evt_tmp2 = u8buf[u8bufp++];
+                        if(evt_tmp2 & 128){
+                            evt_tmp1=s.substring(sp,sp+=u8buf[u8bufp++]);
+                            evt[evt_tmp2&4294967167]=evt_tmp1;
+                            return evt_tmp1;
+                        }
+                        else{
+                            return evt[evt_tmp2&4294967167];
+                        }
+                    }let s = "";let lsp,sp,sl; let c = new TextDecoder();
+            let bubbles,value,id,ptr,len,field,event_name,ns;
             export function create(r){
                 d=r;
             }
@@ -219,13 +219,13 @@ let m,p,ls,d,t,op,i,e,z,metaflags;
                     ls=m.getUint32(d+12*4,true);
                 }
                 p=ls;
-                if ((metaflags>>>5)&1){
-                u8buf=new Uint8Array(m.buffer,m.getUint32(d+5*4,true))
-            }
-            u8bufp=0;if ((metaflags>>>3)&1){
+                if ((metaflags>>>3)&1){
                 u32buf=new Uint32Array(m.buffer,m.getUint32(d+3*4,true))
             }
-            u32bufp=0;if (metaflags&1){
+            u32bufp=0;if ((metaflags>>>5)&1){
+                u8buf=new Uint8Array(m.buffer,m.getUint32(d+5*4,true))
+            }
+            u8bufp=0;if (metaflags&1){
                 lsp = m.getUint32(d+1*4,true);
             }
             if ((metaflags>>>2)&1) {
