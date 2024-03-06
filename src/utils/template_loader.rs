6df66d975b6ce8ad6afe77168beb6ae48a35b905
mrxiaozhuox <mrxzx.info@gmail.com>
2024-03-06 19:26:34 +0000

    Update new to output generated at 6de80c4

--- src/utils/template_loader.rs
+++ src/utils/template_loader.rs
@@ -2,5 +2,9 @@ use std::collections::HashMap;
 pub fn loader() -> HashMap<String, karaty_blueprint::Templates> {
     let mut templates: HashMap<String, karaty_blueprint::Templates> = HashMap::new();
     templates.insert("karaty_template".to_string(), karaty_template::export());
+    templates.insert(
+        "karaty_docsite_template".to_string(),
+        karaty_docsite_template::export(),
+    );
     templates
 }
