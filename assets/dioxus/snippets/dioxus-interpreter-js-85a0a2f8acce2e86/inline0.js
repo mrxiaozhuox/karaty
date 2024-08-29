be65a9e4ce9b66c3bf974e73a7d5534a712af676
mrxiaozhuox <mrxzx.info@gmail.com>
2024-08-29 06:46:25 +0000

    Deploying to gh-pages from @ mrxiaozhuox/karaty@36fafa7d047491fb9be2d03fdaaaea84b5e1954a 🚀

--- assets/dioxus/snippets/dioxus-interpreter-js-85a0a2f8acce2e86/inline0.js
+++ assets/dioxus/snippets/dioxus-interpreter-js-85a0a2f8acce2e86/inline0.js
@@ -169,7 +169,19 @@ let m,p,ls,d,t,op,i,e,z,metaflags;
       function truthy(val) {
         return val === "true" || val === true;
       }
-    const attr = [];
+    let u32buf,u32bufp;const ns_cache = [];
+                    let ns_cache_tmp1, ns_cache_tmp2;
+                    function get_ns_cache() {
+                        ns_cache_tmp2 = u8buf[u8bufp++];
+                        if(ns_cache_tmp2 & 128){
+                            ns_cache_tmp1=s.substring(sp,sp+=u8buf[u8bufp++]);
+                            ns_cache[ns_cache_tmp2&4294967167]=ns_cache_tmp1;
+                            return ns_cache_tmp1;
+                        }
+                        else{
+                            return ns_cache[ns_cache_tmp2&4294967167];
+                        }
+                    }const attr = [];
                     let attr_tmp1, attr_tmp2;
                     function get_attr() {
                         attr_tmp2 = u8buf[u8bufp++];
@@ -193,20 +205,8 @@ let m,p,ls,d,t,op,i,e,z,metaflags;
                         else{
                             return evt[evt_tmp2&4294967167];
                         }
-                    }const ns_cache = [];
-                    let ns_cache_tmp1, ns_cache_tmp2;
-                    function get_ns_cache() {
-                        ns_cache_tmp2 = u8buf[u8bufp++];
-                        if(ns_cache_tmp2 & 128){
-                            ns_cache_tmp1=s.substring(sp,sp+=u8buf[u8bufp++]);
-                            ns_cache[ns_cache_tmp2&4294967167]=ns_cache_tmp1;
-                            return ns_cache_tmp1;
-                        }
-                        else{
-                            return ns_cache[ns_cache_tmp2&4294967167];
-                        }
-                    }let s = "";let lsp,sp,sl; let c = new TextDecoder();let u32buf,u32bufp;
-            let id,ns,event_name,field,value,len,ptr,bubbles;
+                    }let s = "";let lsp,sp,sl; let c = new TextDecoder();
+            let len,id,ns,field,bubbles,ptr,value,event_name;
             export function create(r){
                 d=r;
             }
@@ -219,7 +219,10 @@ let m,p,ls,d,t,op,i,e,z,metaflags;
                     ls=m.getUint32(d+12*4,true);
                 }
                 p=ls;
-                if ((metaflags>>>5)&1){
+                if ((metaflags>>>3)&1){
+                u32buf=new Uint32Array(m.buffer,m.getUint32(d+3*4,true))
+            }
+            u32bufp=0;if ((metaflags>>>5)&1){
                 u8buf=new Uint8Array(m.buffer,m.getUint32(d+5*4,true))
             }
             u8bufp=0;if (metaflags&1){
@@ -248,10 +251,7 @@ let m,p,ls,d,t,op,i,e,z,metaflags;
                     s = c.decode(new DataView(m.buffer, lsp, sl));
                 }
             }
-            sp=0;if ((metaflags>>>3)&1){
-                u32buf=new Uint32Array(m.buffer,m.getUint32(d+3*4,true))
-            }
-            u32bufp=0;
+            sp=0;
                 for(;;){
                     op=m.getUint32(p,true);
                     p+=4;
